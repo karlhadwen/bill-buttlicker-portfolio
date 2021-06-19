805baa9fab890b24dacedbb3ab48134f867c08bc
Karl Hadwen <karlhadwen@gmail.com>
2021-06-19 01:44:50 +0100

    small changes + preview

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ function onClickNavHandler() {
   for (const link of document.querySelectorAll("nav li a")) {
     link.addEventListener('click', function(event) {
       // hide all containers
-      document.querySelectorAll("nav li a").forEach(el => document.querySelector(`.${el.getAttribute('href').substring(1)}`).style.display = 'none');
+      document.querySelectorAll("nav li a")
+        .forEach(el => document.querySelector(`.${el.getAttribute('href').substring(1)}`).style.display = 'none');
 
       // remove active class
-      document.querySelectorAll("nav li a").forEach(el => el.classList.remove('active'));
+      document.querySelectorAll("nav li a")
+        .forEach(el => el.classList.remove('active'));
 
       // get the link that was clicked and target that container
       document.querySelector(`.${event.currentTarget.getAttribute('href').substring(1)}`).style.display = "flex";
